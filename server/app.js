f71669c79755febcd74c9176bf6fa1306b809fb1
ke <wannngkee@gmail.com>
2021-01-27 16:45:04 +0800

    update api

--- server/app.js
+++ server/app.js
@@ -19,28 +19,98 @@ const noteSchema = {
 
 const Note = mongoose.model("Note", noteSchema);
 
-app.get("/notes", function (req, res) {
-  Note.find(function (err, foundNotes) {
+//////////////////////////////////////Requesting Targetting All Notes///////////////////////////
+
+app.route("/notes")
+
+  .get(function (req, res) {
+    Note.find(function (err, foundNotes) {
+      if (!err) {
+        res.send(foundNotes);
+      } else {
+        res.send(err);
+      }
+    })
+  })
+  
+  .post(function (req, res) {
+    const newNote = new Note({
+      title: req.body.title,
+      content: req.body.content
+    })
+    newNote.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new note!");
+      } else {
+        res.send(err);
+      }
+    })
+  })
+
+  .delete (function (req, res) {
+  Note.deleteMany(function (err) {
     if (!err) {
-      res.send(foundNotes);
+      res.send("Successfully deleted all notes");
     } else {
       res.send(err);
     }
-  });
+  })
 });
 
-app.post("/notes", function(req,res){
-  const newNote = new Note({
-    title: req.body.title,
-    content: req.body.content
+//////////////////////////////////////Requesting Targetting A Specific Note///////////////////////////
+app.route("/notes/:noteTitle")
+
+  .get(function (req, res) {
+    Note.findOne({ title: req.params.noteTitle }, function (err, foundNote) {
+      if (foundNote) {
+        res.send(foundNote);
+      } else {
+        res.send("No notes matching that id was found.")
+      }
+    })
   })
-  newNote.save(function(err){
-    if (!err){
-      res.send("Successfully added a new note!");
-    } else(
-      res.send(err);
+
+  .put(function (req, res) {
+    Note.update(
+      { title: req.params.noteTitle },
+      { title: req.body.title, connect: req.body.content },
+      { overwrite: true },
+      function (err) {
+        if (!err) {
+          res.send("Successfully update note.")
+        } else {
+          res.send(err)
+        }
+      }
     )
-  });
-})
+  })
+
+  .patcch(function(req, res){
+  Note.update(
+    { title: req.params.noteTitle },
+    { $set: req.body },
+    function (err) {
+      if (!err) {
+        res.send("Successfully updated note.")
+      } else {
+        res.send(err)
+      }
+    }
+    )
+  })
+  
+  .delete (function (req, res) {
+  Note.deleteOne(
+    { title: req.params.articleTitle },
+    function (err) {
+      if (!err) {
+        res.send("Successfully deleted the corresponding note")
+      } else {
+        res.send(err)
+      }
+    }
+  )
+});
+
 
 app.listen(8000, () => console.log("server running on port 8000"));
