d4ba483079efad04e093b048fa7ff02899d85851
wangke <wannngkee@gmail.com>
2021-02-19 21:32:41 +0800

    bug fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,7 @@ function auth(req, res, next) {
 
   //check for token
   if (!token) {
-    res.status(401).send("No token, authorization denied")
+    return res.status(401).send("No token, authorization denied")
   }
   try {
     //verify token
