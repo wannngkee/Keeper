a0f6bea4f1fbeca2209f2829cad9a4daff31a2ed
wangke <wannngkee@gmail.com>
2021-06-02 16:44:17 +0800

    feat(l): remove console message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,6 @@ function App() {
 
   return (
     <>
-      {/* <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} /> */}
       <Route exact path="/" component={Home} />
     </>
   );
