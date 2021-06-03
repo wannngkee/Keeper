432486b613fc67fb8f8b248b7519b505d5b20f9a
wangke <wannngkee@gmail.com>
2021-06-03 13:55:53 +0800

    test gh-pages

---
+++ client/build/precache-manifest.0a659a0b988bf286f0cb4f38a533ee4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791393c486e36d65827d4ac86c5b7d8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9df683a7782d9524ad2",
+    "url": "./static/js/2.7c68ef63.chunk.js"
+  },
+  {
+    "revision": "65ae6629c4b9ed98e79b",
+    "url": "./static/js/main.8d6a965c.chunk.js"
+  },
+  {
+    "revision": "dcdd92988de4a14565b7",
+    "url": "./static/js/runtime-main.42e84b57.js"
+  }
+]);
