3ea9bf5f87191e7a6360254049027b39b6af70d8
wangke <wannngkee@gmail.com>
2021-06-03 10:53:53 +0800

    feat(l): add patch request

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import DoneOutlineOutlinedIcon from "@material-ui/icons/DoneOutlineOutlined";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Note = (props) => {
+  const { user, isAuthenticated } = useAuth0();
   const [done, setDone] = useState(true);
   const [title, setTitle] = useState(props.note.title);
   const [content, setContent] = useState(props.note.content);
@@ -20,6 +23,23 @@ const Note = (props) => {
         newNotes[props.id] = newNote;
         return newNotes;
       });
+      if (isAuthenticated) {
+        const id = props.id[0];
+        axios
+          .patch(
+            `https://keeper-mern.herokuapp.com/notes/${user.email}/${id}`,
+            {
+              title: newNote.title,
+              content: newNote.content,
+            }
+          )
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     }
   };
   const handleTitle = (e) => {
@@ -34,7 +54,13 @@ const Note = (props) => {
       <>
         <input value={title} onChange={handleTitle} disabled={done} />
         {done ? null : <hr />}
-        <textarea value={content} onChange={handleContent} disabled={done} />
+        <textarea
+          rows={4}
+          value={content}
+          onChange={handleContent}
+          disabled={done}
+          style={{ width: 210 }}
+        />
       </>
       <button onClick={handleDelete}>
         <DeleteIcon />
